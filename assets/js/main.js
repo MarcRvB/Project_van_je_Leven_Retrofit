3003f96d1634e5ed3f0d07d4c14a7a507d45edca
Spoochy <jordeytje@live.nl>
2020-11-04 12:19:06 +0100

    nieuwe kaart
    run de command
    npm start
    of
    npm build
    
    om het te openen

---
+++ assets/js/main.js
@@ -0,0 +1,58 @@
+import 'ol/ol.css';
+import Map from 'ol/Map';
+import OSM from 'ol/source/OSM';
+import TileLayer from 'ol/layer/Tile';
+import TileWMS from 'ol/source/TileWMS';
+import View from 'ol/View';
+
+var layers = [
+    new TileLayer({
+        source: new OSM(),
+    }),
+    new TileLayer({
+        source: new TileWMS({
+            url: 'http://marcrvb.nl/geoserver/Hoogbouw_basic/wms',
+            params: {'LAYERS': 'BAG_Hoogbouw_V2 VVE_Adressen', 'TILED': true},
+            serverType: 'geoserver'
+            // Countries have transparency, so do not fade tiles:
+        }),
+    })];
+
+var map = new Map({
+    layers: layers,
+    target: 'map',
+    view: new View({
+        center: ol.proj.fromLonLat([5.387827, 52.156113]),
+        zoom: 8
+    }),
+});
+
+
+map.on('singleclick', function (evt) {
+    document.getElementById('info').innerHTML = '';
+    var viewResolution = /** @type {number} */ (view.getResolution());
+    var url = wmsSource.getFeatureInfoUrl(
+        evt.coordinate,
+        viewResolution,
+        'EPSG:3857',
+        {'INFO_FORMAT': 'text/html'}
+    );
+    if (url) {
+        fetch(url)
+            .then(function (response) { return response.text(); })
+            .then(function (html) {
+                document.getElementById('info').innerHTML = html;
+            });
+    }
+});
+
+map.on('pointermove', function (evt) {
+    if (evt.dragging) {
+        return;
+    }
+    var pixel = map.getEventPixel(evt.originalEvent);
+    var hit = map.forEachLayerAtPixel(pixel, function () {
+        return true;
+    });
+    map.getTargetElement().style.cursor = hit ? 'pointer' : '';
+});
